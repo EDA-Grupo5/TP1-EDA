350a619061d130d45874a8763b06f94cb5787ed2
AnaBeatrizMAssuh <aaamassuh@gmail.com>
2022-02-22 22:19:51 -0300

    Adicionando codigo gerar arquivos :white_check_mark:

--- tp01-grupo05.c
+++ tp01-grupo05.c
@@ -5,6 +5,11 @@
 int main()
 {
     FILE *tripAd = fopen("tripadvisor_hotel.csv","r");
+    FILE *nota1 = fopen("Nota1.txt", "w+");
+    FILE *nota2 = fopen("Nota2.txt", "w+");
+    FILE *nota3 = fopen("Nota3.txt", "w+");
+    FILE *nota4 = fopen("Nota4.txt", "w+");
+    FILE *nota5 = fopen("Nota5.txt", "w+");
     
     if(tripAd == NULL){
         printf("Erro ao abrir o Arquivo");
@@ -26,11 +31,27 @@ int main()
 
         rating = atoi(sp);
   
-        printf("\n %s \n %d", review, rating);
+        //printf("\n %s \n %d", review, rating);
 
+        if(rating == 1){
+            fprintf(nota1, "%s\n", review);
+            }else if(rating == 2) {
+                fprintf(nota2, "%s\n", review);
+                }else if(rating == 3) {
+                    fprintf(nota3, "%s\n", review);
+                    }else if(rating == 4) {
+                        fprintf(nota4, "%s\n", review);
+                        }else if(rating == 5) {
+                            fprintf(nota5, "%s\n", review);
+                }
     }
 
 
+    fclose(nota1);
+    fclose(nota2);
+    fclose(nota3);
+    fclose(nota4);
+    fclose(nota5);
     fclose(tripAd);
 
     return 0;
