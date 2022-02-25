50c3be8f7c61a660679637fe6675534a8ec18969
AnaBeatrizMAssuh <aaamassuh@gmail.com>
2022-02-25 16:13:01 -0300

    Problema IDF

--- prepara_idf.h
+++ prepara_idf.h
@@ -5,42 +5,52 @@
 #include <stdlib.h>
 #include <string.h>
 
+
 void encontrarIDF(char readVoca[]){
+
     FILE *vocabuRead = fopen(readVoca, "r");
-    char line[50000];
-    int i=0;
-    int j=0;
-    char palavra[]={' '};
-    int indiceAnterior = 0;
-    int counta =0;
     char *ch;
-    while(!feof(vocabuRead)){
+    int conta = 0;
+    int i = 0;
+    int contador = 0;
+    char contaCaracter[][30] = {' '};
+
+     while(!feof(vocabuRead)){
         ch = fgetc(vocabuRead);
         if(ch == '\n'){
-            counta++;
+            conta++;
         }
     }
-        printf("QTD de Palavras: %d \n", counta-1);
+        printf("QTD de Palavras: %d \n", conta-1);
 
     rewind(vocabuRead);
 
-    while(fgets(line,sizeof(line)*3,vocabuRead) !=NULL){
+    ch = '\0';
 
-        if(line[i] != '\n' || line[i] == '\n'  ){
-            //printf("%s", line);
-            indiceAnterior = i;
-    
-            for(j; j < indiceAnterior-1; j++){
-                palavra[j] = line[j];
-            }
-            j='\0';
-            printf("%s", palavra);
-            
-            
+    while (!feof(vocabuRead)){
+        ch = fgetc(vocabuRead);
+        if(ch == '\n'){
+            i = 0;
+            contador++;
         }
+
+        contaCaracter[contador][i] = ch;
+
         i++;
+
     }
-        fclose(vocabuRead);
+    rewind(vocabuRead);
+
+    for(int k = 0; k < contador; k++){
+        for(int g = 0; g < 30; g++){
+            printf("%c", contaCaracter[k][g]);
+        }
+    }
+
+
+    fclose(vocabuRead);
+
+
 }
 
 #endif
