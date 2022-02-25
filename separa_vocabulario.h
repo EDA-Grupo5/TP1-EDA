4a3fc07e8d5c273abb068c8b8ed4716dd071c28e
Gabriel <gabrielfsacademico@gmail.com>
2022-02-25 11:55:33 -0300

    Consertando separação de IDF

--- separa_vocabulario.h
+++ separa_vocabulario.h
@@ -7,28 +7,25 @@
 
 void separa_vocabulario(char arqLido[], char vocaArq[]){
     
-    FILE *notar1 = fopen(arqLido, "r");
-    FILE *voc1 = fopen(vocaArq, "w+");
+    FILE *notar = fopen(arqLido, "r");
+    FILE *voc = fopen(vocaArq, "w+");
     char line[22000];
 
-    if(notar1 == NULL){
+    if(notar == NULL){
         printf("Erro ao abrir o arquivo");
         exit(0);
     }
 
-    while(fgets(line,22000,notar1) !=NULL){
-        for(int i = 0; line[i]!='\0' && line[i]!='\n'; i++){
-            if(line[i] == '.' || line[i] == ',' || line[i] == '\\' || line[i] == '/' && line[i-1] != ',' && line[i-1] != ' ' && line[i-1] != '.'){
-                fputs("\n", voc1);
+    while(fgets(line,22000,notar) !=NULL){
+        for(int i = 0; line[i]!='\n'; i++){
+            if(line[i]!= ' ' && line[i]!= ',' && line[i] != '.' && line[i]!= '!' && line[i] != '?' && line[i] != '\\' && line[i] != '/'){
+                fputc(line[i], voc);
             }
 
-            if(line[i]!= ' ' && line[i]!= ',' && line[i] != '.' && line[i]!= '!' && line[i] != '?' && line[i] != '\\' && line[i] != '/' && line[i] != '-' ){
-                fputc(line[i], voc1);
+            if(line[i]== ' ' || line[i]=='\0'){
+                fputs("\n", voc);
             }   
 
-            if(line[i]== ' ' && line[i] != '.' && line[i] != ',' && line[i-1] != ',' && line[i-1] != ' ' && line[i-1] != '.' ){
-                fputs("\n", voc1);
-            }
         }
     }
 
